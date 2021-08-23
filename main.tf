a7c1f542a7128894a0ae96ca414506333ef465a0
himavanth <himavanthkj@gmail.com>
2021-08-23 05:05:35 +0000

    new updates

--- main.tf
+++ main.tf
@@ -7,53 +7,49 @@ data "aws_availability_zones" "azs" {
 }
 resource "aws_vpc" "web_vpc" {
   cidr_block = "10.0.0.0/16"
-  tags  = {
-      Name  = "main"
+  tags = {
+    Name = "main"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.web_vpc.id
-  availability_zone = data.aws_availability_zones.azs.names
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.web_vpc.id
+  availability_zone = data.aws_availability_zones.azs.names[0]
 }
 
-resource "aws_security_group" "web-sg" {
+resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.web_vpc.id
-  ingress = [
-      {
-          description = "TLS from VPC"
-          from_port = 80
-          to_port = 80
-          protocol = "tcp"
-          cidr_blocks = [aws_vpc.web_vpc.cidr_block]
-      }
-  ]    
-  egress = [
-      {
-          from_port = 0
-          to_port = 0
-          protocol = "-1"
-          cidr_blocks = ["0.0.0.0"]
-      }
-  ]
-    tags = {
-      Name = "allow_tls"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.web_vpc.cidr_block]
   }
-}   
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
 
 data "aws_ami" "my_ami" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 resource "aws_instance" "web_app" {
-  count = 4
-  ami  = data.aws_ami.my_ami.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-  subnet_id = aws_subnet.public_subnet.id
+  count                  = 4
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  subnet_id              = aws_subnet.public_subnet.id
 }
