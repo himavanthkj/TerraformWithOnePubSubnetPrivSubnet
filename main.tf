f964a1f71d0ab110d1c76c195f22a27331c8f340
himavanth <himavanthkj@gmail.com>
2021-08-23 06:04:27 +0000

    adding subnet update

--- main.tf
+++ main.tf
@@ -21,25 +21,23 @@ resource "aws_subnet" "public_subnet" {
 
 resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.web_vpc.id
-  ingress
-      {
-          description = "TLS from VPC"
-          from_port = 80
-          to_port = 80
-          protocol = "tcp"
-          cidr_blocks = [aws_vpc.web_vpc.cidr_block]
-      }
-  egress
-      {
-          from_port = 0
-          to_port = 0
-          protocol = "-1"
-          cidr_blocks = ["0.0.0.0"]
-      }
-    tags = {
-      Name = "allow_tls"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.web_vpc.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
 }
-
 data "aws_ami" "my_ami" {
   most_recent = true
   owners      = ["amazon"]
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,6 @@
 output "instance_id" {
-    value = [aws_instance.web_app.id]
-  
+  value = aws_instance.web_app.*.id
+}
+output "subnet" {
+  value = aws_subnet.public_subnet.id
 }
